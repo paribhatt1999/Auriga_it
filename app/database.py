35a434e3aead602a166579145ad6872c2af805af
Pari Bhattacharya <2023btechaidspari15664@poornima.edu.in>
2026-09-16 15:27:47 +0530

    Set up SQLite database and session dependency

--- app/database.py
+++ app/database.py
@@ -1,4 +1,18 @@
-from sqlmodel import create_engine
+from collections.abc import Generator
 
-DATABASE_URL = "sqlite:///./streak_keeper.db"
-engine = create_engine(DATABASE_URL, echo=True)
+from sqlmodel import Session, SQLModel, create_engine
+
+DATABASE_URL = "sqlite:///./streaks.db"
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+
+def create_db_and_tables() -> None:
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
